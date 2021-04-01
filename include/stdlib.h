cda32d511c65a777ee79cb1ef7f7168c41b7a12b
Claudio Jeker <claudio@openbsd.org>
2021-04-01 14:54:58 +0200

    Add setproctitle() compat code.

--- include/stdlib.h
+++ include/stdlib.h
@@ -20,6 +20,11 @@ void *recallocarray(void *, size_t, size_t, size_t);
 void freezero(void *ptr, size_t sz);
 #endif
 
+#ifndef HAVE_SETPROCTITLE
+void compat_init_setproctitle(int argc, char *argv[]);
+void setproctitle(const char *fmt, ...);
+#endif
+
 #ifndef HAVE_STRTONUM
 long long strtonum(const char *nptr, long long minval,
 		long long maxval, const char **errstr);
