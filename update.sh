1c5a3e65a781a7afac047d5043d9ad90a1590d06
Theo Buehler <tb@openbsd.org>
2023-06-29 12:39:43 +0200

    Update build harness for log.c removal

--- update.sh
+++ update.sh
@@ -61,7 +61,7 @@ ${CP} "${libutil_src}/imsg-buffer.c" compat/
 (cd compat; ${PATCH} -p0 < "${patches}/patch-imsg.c")
 
 for i in as.c aspa.c cert.c cms.c crl.c encoding.c extern.h filemode.c \
-	gbr.c geofeed.c http.c io.c ip.c json.c json.h log.c main.c mft.c \
+	gbr.c geofeed.c http.c io.c ip.c json.c json.h main.c mft.c \
 	mkdir.c ometric.c ometric.h output-bgpd.c output-bird.c output-csv.c \
 	output-json.c output-ometric.c output.c parser.c print.c \
 	repo.c roa.c rpki-client.8 rrdp.c rrdp.h rrdp_delta.c \
